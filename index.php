887588eeaa4b99c172b56308205be31816f48579
mindziukasss <mindziukasss@gmail.com>
2017-02-23 17:56:19 +0200

    Added validates

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'database.php'; ?>
 
 <?php
-  $query = "SELECT * FROM shouts";
+  $query = "SELECT * FROM shouts ORDER BY id DESC";
   $shouts = mysqli_query($con, $query);
 ?>
 <!DOCTYPE html>
@@ -23,6 +23,9 @@
       </ul>
     </div>
     <div id="input">
+      <?php if (isset($_GET['error'])) : ?>
+        <div class="error"><?php echo $_GET['error']; ?></div>
+      <?php endif; ?>
       <form method="post" action="process.php">
         <input type="text" name="user" placeholder="Enter you Name" />
         <input type="text" name="message" placeholder="You Massage" />
